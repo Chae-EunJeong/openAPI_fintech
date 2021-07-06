52713093ab4332bd1356611034cc6ab31c73aa83
Chae-EunJeong <chaeun.jjj@gmail.com>
2021-07-06 21:37:51 +0900

    header style

--- openbanking/src/component/common/Header.js
+++ openbanking/src/component/common/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styled from "styled-components";
 
+const HeaderBlock = styled.div`
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  font-weight: bold;
+  border-bottom: 1px #dedede solid;
+`;
+
 const Header = ({ title }) => {
     return (
-        <div>
-            {title}
-        </div>
+        <HeaderBlock>
+          <div>{title}</div>
+        </HeaderBlock>
     );
 };
 
